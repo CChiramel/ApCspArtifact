37d4022c157c6f6c72f644047d033fdc5c9123ad
ChrisChiramel <cchiramel6081@stu.d214.org>
2024-10-15 09:27:23 -0500

    Initial Commit

---
+++ ApCspArtifact/ApCspArtifactApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApCspArtifactApp.swift
+//  ApCspArtifact
+//
+//  Created by Chris K. Chiramel on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ApCspArtifactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
